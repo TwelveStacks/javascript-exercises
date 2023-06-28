019929cb12d8d65df2d9eb4964d350887dc1b92c
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-06-27 22:07:45 -0400

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const bookNames = books.map(book => book.title)
+    console.log(bookNames)
+    return bookNames
 };
 
 // Do not edit below this line
